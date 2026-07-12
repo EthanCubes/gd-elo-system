21a8850e5dbefcfb57251815d6cffae798e2aea5
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-12 10:35:38 -0600

    Added code to create a backend app and accept request and send back data.

---
+++ backend/src/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+import loop
+
+data = {
+    "game": "geometry dash",
+    "creator": "robtop"
+    }
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/search")
+def search():
+    name = request.args.get("name")
+    playerData, rating = loop.userSearch(name)
+    return jsonify(playerData)
