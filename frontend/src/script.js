613c26b511953346595ec74f9ae96aac3f0603bf
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-12 15:55:20 -0600

    Added code to display stats gotten from backend.

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -3,16 +3,23 @@ const lucky = document.getElementById("lucky");
 const query = document.getElementById("query");
 
 let playerList;
+let playerData;
+
+const loading = document.getElementById("loading");
+const info = document.getElementById("info");
 
 search.addEventListener("click", function() {searchPlayer(query.value)});
 lucky.addEventListener("click", function() {getUsers()}); // to make sure that users are loaded before they're processed
 
 async function searchPlayer(query) {
+    loading.style.display = "block";
+    info.style.display = "none";
     const url = "http://localhost:5000/api/search?name=" + query;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    return(data);
+    playerData = data;
+    loading.style.display = "none";
+    updateStats();
 }
 
 function luckySearch() {
@@ -22,7 +29,7 @@ function luckySearch() {
 }
 
 function getUsers() {
-    const url = "assets/players.txt"
+    const url = "assets/players.txt";
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -37,4 +44,31 @@ function getUsers() {
         .catch(error => {
             console.error("Error", error.message);
         });
+}
+
+function updateStats() {
+    const name = document.getElementById("name");
+    const elo = document.getElementById("elo");
+    const stars = document.getElementById("stars");
+    const moons = document.getElementById("moons");
+    const secretCoins = document.getElementById("secretCoins");
+    const userCoins = document.getElementById("userCoins");
+    const easyDemons = document.getElementById("easyDemons");
+    const mediumDemons = document.getElementById("mediumDemons");
+    const hardDemons = document.getElementById("hardDemons");
+    const insaneDemons = document.getElementById("insaneDemons");
+    const extremeDemons = document.getElementById("extremeDemons");
+    info.style.display = "block";
+
+    name.innerHTML = playerData[0].name;
+    elo.innerHTML = playerData[1];
+    stars.innerHTML = "Stars: " + playerData[0].stars;
+    moons.innerHTML = "Moons: " + playerData[0].moons;
+    secretCoins.innerHTML = "Secret Coins: " + playerData[0].secretCoins;
+    userCoins.innerHTML = "User Coins: " + playerData[0].userCoins;
+    easyDemons.innerHTML = "Easy Demons: " + playerData[0].easyDemons;
+    mediumDemons.innerHTML = "Medium Demons: " + playerData[0].mediumDemons;
+    hardDemons.innerHTML = "Hard Demons: " + playerData[0].hardDemons;
+    insaneDemons.innerHTML = "Insane Demons: " + playerData[0].insaneDemons;
+    extremeDemons.innerHTML = "Extreme Demons: " + playerData[0].extremeDemons;
 }
