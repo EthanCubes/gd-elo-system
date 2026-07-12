76076a7c322b323f787c7427f4d3cea5a64c77a3
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-12 00:59:10 -0600

    Moved main loop code from main.py to newly created file loop.py

---
+++ backend/src/loop.py
@@ -0,0 +1,14 @@
+import serverData
+
+def userSearch():
+    name = input("Enter the name of the player you want to search up: ")
+    playerData = serverData.getDataByName(name)
+    print(playerData)
+    if playerData == "errorCode1":
+        print("Player not found. This may be because the player does not exist or because you aren't connected to the internet")
+
+    rating = serverData.calculateRating(playerData)
+    print(rating)
+
+while True:
+    userSearch()
