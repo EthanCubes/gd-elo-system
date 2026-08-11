083552586d11047114eb71da19142d86ed86e237
EthanCubes <ethanshangyilin@protonmail.com>
2026-08-10 23:39:58 -0600

    Fixed a bunch of issue including missing quotes and unprocessed numbers

--- src/calculate_elo.py
+++ src/calculate_elo.py
@@ -1,14 +1,18 @@
+from fetch_server_data import get_data_by_name
+
 def calculate_elo(name):
     data = get_data_by_name(name)
     rating = 0
-    rating += int(data[stars])
-    rating += int(data[moons])
-    rating += 3*int(data[secretCoins])
-    rating += 3*int(data[usercoins])
-    demons = data[demons]
+    rating += int(data["stars"])
+    rating += int(data["moons"])
+    rating += 3*int(data["secretCoins"])
+    rating += 3*int(data["usercoins"])
+    demons = data["demons"]
     total_demons = 0;
+    # Imma have to write a function to parse this for some reason (or not I can always just write it locally
+    demons = demons.split(",")
     for i in range(10):
-        total_demons += demons[i]
+        total_demons += int(demons[i])
     rating += 10*int(total_demons)
     rating += 10*int(demons[0]) + 10*int(demons[5])
     rating += 20*int(demons[1]) + 20*int(demons[6])
